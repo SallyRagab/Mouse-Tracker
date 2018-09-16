e3d1de7b11677503dc82b8a068f76f67e0007ada
Sally Ragab <sallyragab31@gmail.com>
2018-09-16 20:17:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 const AREA = document.body;
 const CIRCLE = document.querySelector('.circle');
+const CIRCLETWO = document.querySelector('.circle2');
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+function mouseCoordinates(e) {
+
+  var horizontalPosition = windowWidth - e.clientX - 26;
+  var verticalPosition = windowHeight - e.clientY - 26;
+
+  CIRCLE.style.left = horizontalPosition + 'px';
+  CIRCLE.style.top = verticalPosition + 'px';
+
+}
+
+function mouseCoordinatesX(e) {
+  var horizontalPosition = windowWidth - e.clientX - 26;
+  var verticalPosition = windowHeight - e.clientY - 26;
+
+  CIRCLETWO.style.right = horizontalPosition + 'px';
+  CIRCLETWO.style.top = verticalPosition + 'px';
+}
+
+function changeColorOnTouch() {
+  CIRCLE.style.backgroundColor = "green";
+  CIRCLE.style.borderColor = "green";
+}
+
+function changeColor() {
+  CIRCLETWO.style.backgroundColor = "black";
+  CIRCLETWO.style.borderColor = "black";
+}
+
+AREA.addEventListener('mousemove', mouseCoordinates, false);
+
+AREA.addEventListener('mousemove', mouseCoordinatesX, false);
+
+CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+
+CIRCLETWO.addEventListener('mouseenter', changeColor, false);
+
+CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+
+CIRCLETWO.addEventListener('mouseleave', function(){CIRCLETWO.removeAttribute("style");}, false);
